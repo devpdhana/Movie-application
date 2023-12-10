dd0e661dc3d69dfc00fa662d44d8b185f3c18f2f
devpdhana <devpdhansekar@gmail.com>
2023-12-10 06:13:00 +0530

    frontend updated

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,9 +1,17 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <Box height={"100%"} width={"100%"} margin={"auto"} marginTop={5}>
+      <Box width="80vw" height={"40vh"} margin={"auto"}>
+        <img width={"100%"} height={"100%"}
+          src="https://lehren.com/wp-content/uploads/2023/10/4-tamil-movies-to-release-in-theatres-this-friday.jpg"
+          alt="img"
+        />
+      </Box>
+    </Box>
+  );
 }
 
 export default Home
