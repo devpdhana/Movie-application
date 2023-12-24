1f95026a006c8cf59bb45415a8ba34c15d2cfb61
devpdhana <devpdhansekar@gmail.com>
2023-12-24 07:38:38 +0530

    Admin creditionals are stored

--- Frontend/src/components/Admin/Admin.jsx
+++ Frontend/src/components/Admin/Admin.jsx
@@ -4,9 +4,15 @@ import { sendAdminLoginRequest } from '../../api-helpers/api-helpers'
 
 const Admin = () => {
 
+  const onResponseReceived = (data)=>{
+    console.log(data)
+    localStorage.setItem("AdminId",data.id)
+    localStorage.setItem("token",data.token)
+  }
+
   const getData = (data)=>{
     console.log("Admin",data)
-    sendAdminLoginRequest(data.inputs).then((res)=>console.log(res)).catch((err)=>console.log(err))
+    sendAdminLoginRequest(data.inputs).then((res)=>onResponseReceived(res)).catch((err)=>console.log(err))
   }
   return (
     <AuthForm onSubmit={getData} isAdmin={true}/>
