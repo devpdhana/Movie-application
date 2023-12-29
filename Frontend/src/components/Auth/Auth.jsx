2be1bd3d413caba9864449d4dd9f0d6fa8fdb03a
devpdhana <devpdhansekar@gmail.com>
2023-12-29 05:51:44 +0530

    user creditionals-stored

--- Frontend/src/components/Auth/Auth.jsx
+++ Frontend/src/components/Auth/Auth.jsx
@@ -3,9 +3,14 @@ import AuthForm from './AuthForm'
 import { sendUserAuthRequest } from '../../api-helpers/api-helpers'
 
 const Auth = () => {
+
+  const onResponseReceived = (data)=>{
+    console.log(data)
+    localStorage.setItem("userId",data.id)
+  }
   const getData = (data)=>{
     console.log("Auth",data)
-    sendUserAuthRequest(data.inputs,data.isSignup).then((res)=>console.log(res)).catch((err)=>console.log(err))
+    sendUserAuthRequest(data.inputs,data.isSignup).then((res)=>onResponseReceived(res)).catch((err)=>console.log(err))
   }
   return (
     <AuthForm onSubmit={getData} isAdmin={false}/>
